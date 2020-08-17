a4ab2373f711708899710cd6b18137bc2cfb8ad2
ksamy90 <ksamy90@aol.com>
2020-08-17 21:36:13 +0300

    using promises for auth data

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,9 +2,14 @@ const User = require("../models/User");
 
 exports.login = function (req, res) {
   let user = new User(req.body);
-  user.login(function (result) {
-    res.send(result);
-  });
+  user
+    .login()
+    .then(function (result) {
+      res.send(result);
+    })
+    .catch(function (err) {
+      res.send(err);
+    });
 };
 
 exports.register = function (req, res) {
--- models/User.js
+++ models/User.js
@@ -55,18 +55,20 @@ User.prototype.validate = function () {
   }
 };
 
-User.prototype.login = function (callback) {
-  this.cleanUp();
-  usersCollection.findOne(
-    { username: this.data.username },
-    (err, attemptedUser) => {
-      if (attemptedUser && attemptedUser.password == this.data.password) {
-        callback("Congrats!!!");
-      } else {
-        callback("Invalid username/password");
+User.prototype.login = function () {
+  return new Promise((resolve, reject) => {
+    this.cleanUp();
+    usersCollection.findOne(
+      { username: this.data.username },
+      (err, attemptedUser) => {
+        if (attemptedUser && attemptedUser.password == this.data.password) {
+          resolve("Congrats!!!");
+        } else {
+          reject("Invalid username/password");
+        }
       }
-    }
-  );
+    );
+  });
 };
 
 User.prototype.register = function () {
