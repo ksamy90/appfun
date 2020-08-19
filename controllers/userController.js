e031fa7867fa0940a794712b9550ee28f0869145
ksamy90 <ksamy90@aol.com>
2020-08-19 14:24:11 +0300

    use flash messaging for login errors

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,8 +11,10 @@ exports.login = function (req, res) {
       });
     })
     .catch(function (err) {
-      // res.send(err);
-      res.redirect("/");
+      req.flash("errors", err);
+      req.session.save(function () {
+        res.redirect("/");
+      });
     });
 };
 
@@ -36,6 +38,6 @@ exports.home = function (req, res) {
   if (req.session.user) {
     res.render("home-dashboard", { username: req.session.user.username });
   } else {
-    res.render("home-guest");
+    res.render("home-guest", { errors: req.flash("errors") });
   }
 };
