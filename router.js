8629e0eb40ee012b641de3766cfb2820d6318ebb
ksamy90 <ksamy90@aol.com>
2020-08-24 12:15:29 +0300

    protect loggedin users posts

--- router.js
+++ router.js
@@ -30,7 +30,15 @@ router.post(
 );
 
 router.get("/post/:id", postController.viewSingle);
-router.get("/post/:id/edit", postController.viewEditScreen);
-router.post("/post/:id/edit", postController.edit);
+router.get(
+  "/post/:id/edit",
+  userController.mustBeLoggedIn,
+  postController.viewEditScreen
+);
+router.post(
+  "/post/:id/edit",
+  userController.mustBeLoggedIn,
+  postController.edit
+);
 
 module.exports = router;
