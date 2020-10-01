f599dd9b5768a15f7a3b2078c017a57e2c9f39a7
ksamy90 <ksamy90@aol.com>
2020-10-01 16:39:54 +0300

    implement sockets among users

--- frontend-js/modules/chat.js
+++ frontend-js/modules/chat.js
@@ -4,12 +4,19 @@ export default class Chat {
     this.chatWrapper = document.querySelector("#chat-wrapper");
     this.openIcon = document.querySelector(".header-chat-icon");
     this.injectHTML();
+    this.chatLog = document.querySelector("#chat");
+    this.chatField = document.querySelector("#chatField");
+    this.chatForm = document.querySelector("#chatForm");
     this.closeIcon = document.querySelector(".chat-title-bar-close");
     this.events();
   }
 
   // events
   events() {
+    this.chatForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      this.sendMessageToServer();
+    });
     this.openIcon.addEventListener("click", () => {
       this.showChat();
     });
@@ -19,6 +26,27 @@ export default class Chat {
   }
 
   // methods
+  sendMessageToServer() {
+    this.socket.emit("chatMessageFromBrowser", {
+      message: this.chatField.value,
+    });
+    this.chatLog.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="chat-self">
+        <div class="chat-message">
+          <div class="chat-message-inner">
+            ${this.chatField.value}
+          </div>
+        </div>
+        <img class="chat-avatar avatar-tiny" src="${this.avatar}">
+      </div>
+    `
+    );
+    this.chatLog.scrollTop = this.chatLog.scrollHeight;
+    this.chatField.value = "";
+    this.chatField.focus();
+  }
   hideChat() {
     this.chatWrapper.classList.remove("chat--visible");
   }
@@ -28,10 +56,32 @@ export default class Chat {
     }
     this.openedYet = true;
     this.chatWrapper.classList.add("chat--visible");
+    this.chatField.focus();
   }
 
   openConnection() {
     this.socket = io();
+    this.socket.on("welcome", (data) => {
+      this.username = data.username;
+      this.avatar = data.avatar;
+    });
+    this.socket.on("chatMessageFromServer", (data) => {
+      this.displayMessageFromServer(data);
+    });
+  }
+
+  displayMessageFromServer(data) {
+    this.chatLog.insertAdjacentHTML(
+      "beforeend",
+      `<div class="chat-other">
+      <a href="/profile/${data.username}"><img class="avatar-tiny" src="${data.avatar}"></a>
+      <div class="chat-message"><div class="chat-message-inner">
+        <a href="/profile/${data.username}"><strong>${data.username}:</strong></a>
+        ${data.message}
+      </div></div>
+    </div>`
+    );
+    this.chatLog.scrollTop = this.chatLog.scrollHeight;
   }
 
   injectHTML() {
