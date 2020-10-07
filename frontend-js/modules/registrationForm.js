ed15c0810984c0cb0508b0b40dd9855ef9dfac14
ksamy90 <ksamy90@aol.com>
2020-10-07 09:36:04 +0300

    set validations for regform

--- frontend-js/modules/registrationForm.js
+++ frontend-js/modules/registrationForm.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export default class RegistrationForm {
   constructor() {
+    this.form = document.querySelector("#registration-form");
     this.allFields = document.querySelectorAll(
       "#registration-form .form-control"
     );
@@ -10,20 +11,59 @@ export default class RegistrationForm {
     this.username.previousValue = "";
     this.email = document.querySelector("#email-register");
     this.email.previousValue = "";
+    this.password = document.querySelector("#password-register");
+    this.password.previousValue = "";
+    this.username.isUnique = false;
+    this.email.isUnique = false;
     this.events();
   }
 
   // Events
   events() {
+    this.form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      this.formSubmitHandler();
+    });
     this.username.addEventListener("keyup", () => {
       this.isDifferent(this.username, this.usernameHandler);
     });
     this.email.addEventListener("keyup", () => {
       this.isDifferent(this.email, this.emailHandler);
     });
+    this.password.addEventListener("keyup", () => {
+      this.isDifferent(this.password, this.passwordHandler);
+    });
+
+    this.username.addEventListener("blur", () => {
+      this.isDifferent(this.username, this.usernameHandler);
+    });
+    this.email.addEventListener("blur", () => {
+      this.isDifferent(this.email, this.emailHandler);
+    });
+    this.password.addEventListener("blur", () => {
+      this.isDifferent(this.password, this.passwordHandler);
+    });
   }
 
   // Methods
+  formSubmitHandler() {
+    this.usernameImmediately();
+    this.usernameAfterDelay();
+    this.emailAfterDelay();
+    this.passwordImmediately();
+    this.passwordAfterDelay();
+
+    if (
+      this.username.isUnique &&
+      !this.username.errors &&
+      this.email.isUnique &&
+      !this.email.errors &&
+      !this.password.errors
+    ) {
+      this.form.submit();
+    }
+  }
+
   isDifferent(el, handler) {
     if (el.previousValue != el.value) {
       handler.call(this);
@@ -40,6 +80,37 @@ export default class RegistrationForm {
     }, 800);
   }
 
+  passwordHandler() {
+    this.password.errors = false;
+    this.passwordImmediately();
+    clearTimeout(this.password.timer);
+    this.password.timer = setTimeout(() => {
+      this.passwordAfterDelay();
+    }, 800);
+  }
+
+  passwordImmediately() {
+    if (this.password.value.length > 50) {
+      this.showValidationError(
+        this.password,
+        "Password cannot exceed 50 characters."
+      );
+    }
+
+    if (!this.password.errors) {
+      this.hideValidationError(this.password);
+    }
+  }
+
+  passwordAfterDelay() {
+    if (this.password.value.length < 12) {
+      this.showValidationError(
+        this.password,
+        "Password must be at least 12 characters."
+      );
+    }
+  }
+
   emailHandler() {
     this.email.errors = false;
     clearTimeout(this.email.timer);
@@ -54,6 +125,26 @@ export default class RegistrationForm {
         this.email,
         "You must provide a valid email address."
       );
+    }
+
+    if (!this.email.errors) {
+      axios
+        .post("/doesEmailExist", { email: this.email.value })
+        .then((response) => {
+          if (response.data) {
+            this.email.isUnique = false;
+            this.showValidationError(
+              this.email,
+              "That email is already being used."
+            );
+          } else {
+            this.email.isUnique = true;
+            this.hideValidationError(this.email);
+          }
+        })
+        .catch(() => {
+          console.log("Please try again later.");
+        });
     }
   }
 
